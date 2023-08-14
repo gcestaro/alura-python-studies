b8f02f6321d8b04c8602114cfee728c5973829a4
Gabriel Cestaro <gabrielcestaro94@gmail.com>
2023-08-14 18:11:10 +0100

    Creating Playlist class

--- oop2/Movie.py
+++ oop2/Movie.py
@@ -38,15 +38,35 @@ class Series(TvShow):
         return f"{self.name} - {self.__seasons} - {self.likes}"
 
 
+class Playlist:
+
+    def __init__(self, name, tv_shows):
+        self.__name = name
+        self.__tv_shows = tv_shows
+
+    def size(self):
+        return len(self.__tv_shows)
+
+
 if __name__ == '__main__':
     avengers = Movie("avengers - infinity war", 2018, 160)
-    breaking_bad = Series("breaking bad", 2008, 5)
+    avengers.like()
 
-    avengers.like()
-    avengers.like()
+    harry_potter_6 = Movie("harry potter and the half-blood prince", 2009, 150)
+    harry_potter_6.like()
+
+    star_wars_ep_4 = Movie("Star Wars - Episode IV – A New Hope", 1977, 180)
+    star_wars_ep_4.like()
+
+    breaking_bad = Series("breaking bad", 2008, 5)
     breaking_bad.like()
 
-    tv_shows = [avengers, breaking_bad]
+    prison_break = Series("prison break", 2005, 5)
+    prison_break.like()
+
+    tv_shows = [avengers, breaking_bad, prison_break, star_wars_ep_4, harry_potter_6]
+
+    weekend_playlist = Playlist("Weekend", tv_shows)
 
     for tv_show in tv_shows:
         print(tv_show)
