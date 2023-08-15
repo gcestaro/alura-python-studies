41007616a3bc2453069f6dffb4ab7200be756ca2
Gabriel Cestaro <gabrielcestaro94@gmail.com>
2023-08-15 11:36:47 +0100

    Rename folder utils to dates, and extract class URLExtractor

--- strings/url_extractor.py
+++ strings/url_extractor.py
@@ -1,32 +1,60 @@
+class URLExtractor:
+    url_parameter_separator = "&"
+
+    def __init__(self, url=""):
+        self.__url = self.__sanityze(url)
+        self.validate_url()
+        self.__base_url = self.__get_base_url()
+        self.__url_parameters = self.__get_url_parameters()
+
+    def get_parameter_value(self, name):
+        parameter_start_index = self.__url_parameters.find(name)
+
+        if parameter_start_index == -1:
+            raise ValueError(f"Parameter {name} not found")
+
+        parameter_value_start_index = parameter_start_index + len(name) + 1
+
+        next_parameter_separator_index = self.__url_parameters.find(self.url_parameter_separator,
+                                                                    parameter_value_start_index)
+
+        if next_parameter_separator_index == -1:
+            return self.__url_parameters[parameter_value_start_index:]
+
+        return self.__url_parameters[parameter_value_start_index:next_parameter_separator_index]
+
+    def __sanityze(self, url):
+        if type(url) == str:
+            return url.strip()
+        return ""
+
+    def validate_url(self):
+        if not self.__url:
+            raise ValueError("Empty URL")
+
+    def __get_base_url(self):
+        question_mark_index = self.__url.find("?")
+
+        if question_mark_index == -1:
+            return self.__url
+
+        return self.__url[:question_mark_index]
+
+    def __get_url_parameters(self):
+        question_mark_index = self.__url.find("?")
+
+        if question_mark_index == -1:
+            return ""
+
+        return self.__url[question_mark_index + 1:]
+
+
 if __name__ == '__main__':
-    url = "https://bytebank.com/exchange?sourceCoin=BRL&targetCoin=USD&amount=100"
+    url = "https://bytebank.com/exchange?sourceCoin=BRL&targetCoin=USD&quantity=100"
 
-    base_url = url[0:29]
-    print(base_url)  # https://bytebank.com/exchange
+    url_extractor = URLExtractor(url)
+    sourceCoin = url_extractor.get_parameter_value("sourceCoin")
+    targetCoin = url_extractor.get_parameter_value("targetCoin")
+    quantity = url_extractor.get_parameter_value("quantity")
 
-    parameters_url = url[30:70]
-    print(parameters_url)  # sourceCoin=BRL&targetCoin=USD&amount=100
-
-    question_mark_index = url.find("?")  # https://docs.python.org/pt-br/3/library/stdtypes.html?highlight=find#str.find
-    print(question_mark_index)  # 29
-
-    print(url[0:question_mark_index])  # https://bytebank.com/exchange
-    print(url[:question_mark_index])  # https://bytebank.com/exchange
-
-    print(url[question_mark_index + 1:])  # sourceCoin=BRL&targetCoin=USD&amount=100
-    print(url[question_mark_index + 1:len(url)])  # sourceCoin=BRL&targetCoin=USD&amount=100
-
-    source_coin_parameter_name = "sourceCoin"
-    source_coin_parameter_value_index = parameters_url.find(source_coin_parameter_name) + len(
-        source_coin_parameter_name) + 1
-
-    parameter_separator = "&"
-    next_parameter_separator_index = parameters_url.find(parameter_separator, source_coin_parameter_value_index)
-
-    if next_parameter_separator_index == -1:
-        source_coin_parameter_value = parameters_url[source_coin_parameter_value_index:]
-    else:
-        source_coin_parameter_value = \
-            parameters_url[source_coin_parameter_value_index:next_parameter_separator_index]
-
-    print(source_coin_parameter_value)
+    print(sourceCoin, targetCoin, quantity)
