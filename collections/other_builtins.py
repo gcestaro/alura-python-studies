d2e8c663b7d981b458baa1c939d18b1d29f62072
Gabriel Cestaro <gabrielcestaro94@gmail.com>
2023-08-15 14:37:52 +0100

    Natural order sorting

--- collections/other_builtins.py
+++ collections/other_builtins.py
@@ -1,13 +1,25 @@
-if __name__ == '__main__':
-    ages = [15, 21, 30, 36, 90, 101]
-
+def _range():
+    global i
     range_of_ages = range(len(ages))  # lazy
-
     print(range_of_ages)
     for i in range_of_ages:
         print(i, ages[i])
 
+
+def _enumerate():
+    global i
     ages_enum = enumerate(ages)  # lazy, list of tuples (index, value)
     print(ages_enum)
     for i, age in ages_enum:  # unpacking tuple
         print(i, age)
+
+
+if __name__ == '__main__':
+    ages = [15, 21, 45, 30, 54, 78, 36, 90, 101]
+
+    # _range()
+    # _enumerate()
+
+    print(sorted(ages))
+    print(sorted(ages, reverse=True))
+    print(ages)
