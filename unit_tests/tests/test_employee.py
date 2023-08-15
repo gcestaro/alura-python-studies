1cb0c4253ae7a5c97ac3491f2f76b33c972b6f92
Gabriel Cestaro <gabrielcestaro94@gmail.com>
2023-08-15 18:35:26 +0100

    Intro to markers

--- unit_tests/tests/test_employee.py
+++ unit_tests/tests/test_employee.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest import mark
 
 from unit_tests.main.employee import Employee
 
@@ -25,6 +26,7 @@ class TestClass:
     def employee_without_bonus(self):
         return Employee(TEST_EMPLOYEE_NAME, TEST_EMPLOYEE_DATE_OF_BIRTH, 10_001)
 
+    @mark.age
     def test_when_age_05_10_2000_should_return_23(self, employee):
         # Given (context)
         expected = 23
@@ -35,22 +37,26 @@ class TestClass:
         # Then (conclusion)
         assert result == expected
 
-    def test_when_name_Unit_Test_return_Cestaro(self, employee):
+    @mark.naming
+    def test_when_name_unit_test_return_test(self, employee):
         expected = "Test"
         result = employee.lastname()
         assert result == expected
 
+    @mark.decrease_salary
     def test_when_decrease_salary_from_100k_return_90k(self, director_employee):
         expected = 90_000
         director_employee.decrease_salary()
         result = director_employee.salary
         assert result == expected
 
+    @mark.bonus_calculation
     def test_when_salary_higher_than_10k_bonus_raise_exception(self, employee_without_bonus):
         with pytest.raises(ValueError):
             result = employee_without_bonus.calculate_bonus()
             assert result
 
+    @mark.bonus_calculation
     def test_when_salary_lower_than_10k_bonus_10_percent(self, employee):
         expected = 100
         result = employee.calculate_bonus()
