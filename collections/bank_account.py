da4817202e1a02aba27fd82584c60c5d9fcb78fe
Gabriel Cestaro <gabrielcestaro94@gmail.com>
2023-08-15 14:21:37 +0100

    Implement __eq__ when using collections

--- collections/bank_account.py
+++ collections/bank_account.py
@@ -9,9 +9,16 @@ class BankAccount(metaclass=ABCMeta):
     def deposit(self, amount):
         self._balance += amount
 
+    @property
+    def code(self):
+        return self._code
+
     @abstractmethod
     def apply_taxes(self):
         pass
+
+    def __eq__(self, other):
+        return self._code == other.code
 
     def __str__(self):
         return f"[>>Code {self._code} Balance {self._balance}]"
